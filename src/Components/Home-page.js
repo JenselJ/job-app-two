170712fb05e228b905ac0d52442217c23a4a2770
JenselJ <saneer2002@gmail.com>
2022-10-26 09:09:47 +1100

    started mobile responsiveness

--- src/Components/Home-page.js
+++ src/Components/Home-page.js
@@ -312,22 +312,24 @@ function HomePage() {
   return (
     <>
       <div className="full-div">
-        <header className="header flex flex-row justify-between mt-8">
-          <div>
-            <div className="text-4xl ml-8 font-bold">JobWorm.net</div>
+        <header className="header sm:justify-between mt-8 flex">
+          <div className="text-center text-4xl sm:ml-8 font-bold mb-2">
+            JobWorm.net
           </div>
-          <div className="flex">
-            <div className="mr-10">
+          <div className="header-child flex">
+            <div>
+              <div className="text-center ml-2 sm:mr-8 mb-3">
+                {user.uid}
+              </div>
+            </div>
+            <div className="mx-auto sm:mr-10">
               <button
                 type="submit"
-                className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={() => setAddJobShow(true)}
               >
                 Add Job
               </button>
-            </div>
-            <div>
-              <div className="mr-8">{user.uid}</div>
             </div>
           </div>
 
